60dad3410b11096f22ade08761b47c57dc9b3336
saboooor <saboor547@gmail.com>
2025-07-10 12:33:37 -0500

    merge files into main

--- frontend/src/components/MenuItems.tsx
+++ frontend/src/components/MenuItems.tsx
@@ -1,9 +1,9 @@
-import { ChevronRight, File, Folder } from "lucide-react";
-import { File as FileType, Folder as FolderType } from "../files";
+import { ChevronRight, FileIcon, FolderIcon } from "lucide-react";
+import { File, Folder } from "../main";
 // @ts-expect-error types not working yet
 import { getClasses } from "@luminescent/ui-react"
 
-export default function MenuItems({ files }: { files: (FolderType | FileType)[] }) {
+export default function MenuItems({ files }: { files: (Folder | File)[] }) {
   return (
     <div className="flex flex-col gap-2">
       {Array.isArray(files) ? (
@@ -11,15 +11,15 @@ export default function MenuItems({ files }: { files: (FolderType | FileType)[] 
           <div
             key={index}
             className={getClasses({
-              "lum-btn gap-2 p-2 lum-bg-transparent": true,
+              "lum-btn gap-2 p-1 lum-bg-transparent rounded-lum-1": true,
               'cursor-pointer': !('files' in file),
             })}
           >
             {'files' in file && <ChevronRight size={16} className="text-gray-400" />}
             {'files' in file ? (
-              <Folder size={20} />
+              <FolderIcon size={20} className="text-orange-300" />
             ) : (
-              <File size={20} className="ml-6" />
+              <FileIcon size={20} className="ml-6 text-blue-300" />
             )}
             <span>{file.name}</span>
             {'files' in file && (
