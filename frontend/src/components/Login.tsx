41031f5b4f5690f768db2f331b57e680955f96d1
Addison Goolsbee <addisongoolsbee@gmail.com>
2025-07-11 00:07:08 -0500

    Fix console errors

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -3,7 +3,6 @@ import { SiGoogle } from "@icons-pack/react-simple-icons";
 const backendUrl = import.meta.env.VITE_ENVIRONMENT === "production"
   ? import.meta.env.VITE_PROD_BACKEND_URL
   : import.meta.env.VITE_BACKEND_URL;
-console.log(backendUrl);
 
 export default function Login() {
   return (
--- frontend/src/components/MenuItems.tsx
+++ frontend/src/components/MenuItems.tsx
@@ -2,34 +2,38 @@ import { ChevronRight, FileIcon, FolderIcon } from "lucide-react";
 import { File, Folder } from "../main";
 // @ts-expect-error types not working yet
 import { getClasses } from "@luminescent/ui-react"
+import React from "react";
 
 export default function MenuItems({ files }: { files: (Folder | File)[] }) {
   return (
     <div className="flex flex-col gap-2">
       {Array.isArray(files) ? (
-        files.map((file, index) => <>
-          <div
-            key={`${file.name}-${index}`}
-            className={getClasses({
-              "lum-btn gap-2 p-1 lum-bg-transparent rounded-lum-1": true,
-              'cursor-pointer': !('files' in file),
-            })}
-          >
-            {'files' in file && <ChevronRight size={16} className="text-gray-400" />}
-            {'files' in file ? (
-              <FolderIcon size={20} className="text-orange-300" />
-            ) : (
-              <FileIcon size={20} className="ml-6 text-blue-300" />
-            )}
-            <span>{file.name}</span>
-            {'files' in file && (
-              <span className="text-gray-500 text-sm ml-auto">
-                {file.files.length} items
-              </span>
-            )}
-          </div>
-          {'files' in file && <MenuItems files={file.files} />}
-        </>)
+        files.map((file, idx) => (
+          <React.Fragment key={file.name + "-" + idx}>
+            <div
+              className={getClasses({
+                "lum-btn gap-2 p-1 lum-bg-transparent rounded-lum-1": true,
+                "cursor-pointer": !("files" in file),
+              })}
+            >
+              {"files" in file && (
+                <ChevronRight size={16} className="text-gray-400" />
+              )}
+              {"files" in file ? (
+                <FolderIcon size={20} className="text-orange-300" />
+              ) : (
+                <FileIcon size={20} className="ml-6 text-blue-300" />
+              )}
+              <span>{file.name}</span>
+              {"files" in file && (
+                <span className="text-gray-500 text-sm ml-auto">
+                  {file.files.length} items
+                </span>
+              )}
+            </div>
+            {"files" in file && <MenuItems files={file.files} />}
+          </React.Fragment>
+        ))
       ) : (
         <div className="text-gray-500">No files found</div>
       )}
